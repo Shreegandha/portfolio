25a72244fc68bc1c3faf879e5e5447ee5dfe75f8
Shreegandha <shreegadha.kulkarni@gmail.com>
2019-11-19 17:41:27 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Shreegandha Kulkarni CSP2]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Kulkarni_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Kulkarni_Projects.html">Projects.html</a></li></br>
+      <li><a href="Kulkarni_Resume.html">Resume.html</a></li></br>
+      <li><a href="Kulkarni_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
